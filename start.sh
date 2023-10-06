a9966d28296e15e3c318c45e270de74e9265718c
Jan Sterba <info@jansterba.com>
2023-10-06 21:24:11 +0200

    starter logging

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 env
 mkdir -pv $TYPESENSE_DATA_DIR
 ls -la $TYPESENSE_DATA_DIR
-touch -v $TYPESENSE_DATA_DIR/.writable-check
+echo Touching
+touch $TYPESENSE_DATA_DIR/.writable-check
+echo Done touching
 ls -la $TYPESENSE_DATA_DIR
 
 ./typesense-server
