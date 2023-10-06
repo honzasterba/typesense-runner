b5ed87d58ea2b462947d2cdfe86b9c2461beafa3
Jan Sterba <info@jansterba.com>
2023-10-06 21:50:34 +0200

    try fixed ports

--- start.sh
+++ start.sh
@@ -3,12 +3,8 @@
 echo Data dir is $DATA_DIR
 rm -vrf $DATA_DIR/typesense
 
-# --api-port	false	Port on which Typesense API service listens. Default: 8108
-# --peering-address	false	Internal IP address to which Typesense peering service binds. If this parameter is not specified, Typesense will attempt to use the first available internal IP.
-# --peering-port	false	Port on which Typesense peering service listens. Default: 8107
-
-export TYPESENSE_API_PORT=$PORT
-export TYPESENSE_PEERING_PORT=$((i+1))
+export TYPESENSE_API_PORT=10000
+export TYPESENSE_PEERING_PORT=10001
 export TYPESENSE_DATA_DIR=$DATA_DIR/typesense
 mkdir -pv $TYPESENSE_DATA_DIR
 
