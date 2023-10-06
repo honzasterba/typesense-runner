e7cd4fee554072d1e797990183305360f45e655b
Jan Sterba <info@jansterba.com>
2023-10-06 22:17:09 +0200

    update build script

--- build-from-source.sh
+++ build-from-source.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/typesense/typesense.git
-
+apt-get update
+apt-get install -y software-properties-common
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install -y g++-10 make git zlib1g-dev m4
@@ -14,7 +14,12 @@ apt-get install apt-transport-https curl gnupg -y
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
 mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt-get update && apt-get install bazel
+apt-get update
+apt-get install -y bazel
+
+git clone https://github.com/typesense/typesense.git
+cd typesense
+git checkout v0.25.1
 
 bazel build @com_google_protobuf//:protobuf_headers
 bazel build @com_google_protobuf//:protobuf_lite
