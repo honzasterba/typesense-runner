dca6fadf4c04847d289f9bd5e93be6558100004f
Jan Sterba <info@jansterba.com>
2023-10-06 21:35:13 +0200

    start clean

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo Data dir is $TYPESENSE_DATA_DIR
+rm -vrf $TYPESENSE_DATA_DIR/*
 mkdir -pv $TYPESENSE_DATA_DIR
 ls -la $TYPESENSE_DATA_DIR
 echo Touching
@@ -9,3 +10,6 @@ echo Done touching
 ls -la $TYPESENSE_DATA_DIR
 
 ./typesense-server
+
+find $TYPESENSE_DATA_DIR
+exit 1
