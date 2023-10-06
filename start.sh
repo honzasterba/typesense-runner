d5a3d47a316750db17249a62fa6834cd5022869f
Jan Sterba <info@jansterba.com>
2023-10-06 21:44:17 +0200

    use data dir var

--- start.sh
+++ start.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-echo Data dir is $TYPESENSE_DATA_DIR
-rm -vrf $TYPESENSE_DATA_DIR/*
-mkdir -pv $TYPESENSE_DATA_DIR
-ls -la $TYPESENSE_DATA_DIR
-echo Touching
-touch $TYPESENSE_DATA_DIR/.writable-check
-echo Done touching
-ls -la $TYPESENSE_DATA_DIR
+echo Data dir is $DATA_DIR
+rm -vrf $DATA_DIR/typesense
 
 # --api-port	false	Port on which Typesense API service listens. Default: 8108
 # --peering-address	false	Internal IP address to which Typesense peering service binds. If this parameter is not specified, Typesense will attempt to use the first available internal IP.
 # --peering-port	false	Port on which Typesense peering service listens. Default: 8107
 
-TYPESENSE_API_PORT=$PORT
-TYPESENSE_PEERING_PORT=$((i+1))
+export TYPESENSE_API_PORT=$PORT
+export TYPESENSE_PEERING_PORT=$((i+1))
+export TYPESENSE_DATADIR=$DATA_DIR/typesense
+mkdir -pv $TYPESENSE_DATA_DIR
 
 ./typesense-server
+
+find $DATA_DIR
